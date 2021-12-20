a3928210a621f460a8ad3ea3a5af8ce54624abbc
Ray <58274552+Ray9090@users.noreply.github.com>
2021-12-20 01:24:14 +0100

    Delete NEWS Service API _ First Solution directory

--- NEWS Service API _ First Solution/news-service/src/main/java/com/newsservice/service/SecurityService.java
+++
@@ -1,7 +0,0 @@
-package com.newsservice.service;
-
-public interface SecurityService {
-
-	boolean isAuthenticated();
-    void autoLogin(String username, String password);
-}
--- NEWS Service API _ First Solution/news-service/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO role(role_id,name) VALUES(1,'ROLE_USER');
-INSERT INTO role(role_id,name) VALUES(2,'ROLE_PUBLISHER');
-INSERT INTO role(role_id,name) VALUES(3,'ROLE_ADMIN');
-INSERT INTO user(user_id,password, username) VALUES(700,'$2a$10$hwtrWB5zZtREenI78estWu4Ze4tIKdGUNlH30bb7Fm.TT91iXqcM6','admin');
-insert into user_role VALUES(700,3);
